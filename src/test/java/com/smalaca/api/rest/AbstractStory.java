2bf8cbb49cd05b9189d4d19de2aa98089d7965d5
smalaca <ab13.krakow@gmail.com>
2018-08-21 08:51:16 +0200

    detailed report generation

--- src/test/java/com/smalaca/api/rest/AbstractStory.java
+++ src/test/java/com/smalaca/api/rest/AbstractStory.java
@@ -2,29 +2,41 @@ package com.smalaca.api.rest;
 
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.io.CodeLocations;
 import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.junit.JUnitStories;
+import org.jbehave.core.reporters.CrossReference;
+import org.jbehave.core.reporters.FilePrintStreamFactory;
 import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 
 import java.util.List;
+import java.util.Properties;
 
 import static java.util.Arrays.asList;
-import static org.jbehave.core.io.CodeLocations.codeLocationFromClass;
 import static org.jbehave.core.reporters.StoryReporterBuilder.Format.CONSOLE;
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.HTML;
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.TXT;
+import static org.jbehave.core.reporters.StoryReporterBuilder.Format.XML;
 
 public abstract class AbstractStory extends JUnitStories {
+    private final CrossReference xref = new CrossReference();
 
     abstract String storyName();
 
     @Override
     public Configuration configuration() {
+        Properties viewResources = new Properties();
+        viewResources.put("decorateNonHtml", "true");
+
         return new MostUsefulConfiguration()
                 .useStoryLoader(new LoadFromClasspath(this.getClass()))
                 .useStoryReporterBuilder(new StoryReporterBuilder()
-                        .withCodeLocation(codeLocationFromClass(this.getClass()))
-                        .withFormats(CONSOLE));
+                        .withCodeLocation(CodeLocations.codeLocationFromClass(this.getClass()))
+                        .withDefaultFormats().withPathResolver(new FilePrintStreamFactory.ResolveToPackagedName())
+                        .withViewResources(viewResources).withFormats(CONSOLE, TXT, HTML, XML)
+                        .withFailureTrace(true).withFailureTraceCompression(true).withCrossReference(xref));
     }
 
     abstract Object stepInstance();
